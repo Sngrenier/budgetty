48ce707fd59d3d9b7f8de63449468884185b0243
Stacy Grenier <sngrenier@gmail.com>
2021-02-10 21:49:10 -0800

    budgetty-lab-Completion-first-pass

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+const REQUEST_BUDGET_DATA = "REQUEST_BUDGET_DATA"
+const ADD_PURCHASE = "ADD_PURCHASE"
+const REMOVE_PURCHSE = "REMOVE_PURCHASE"
+
+export function requestBudgetData(){
+    let data = axios.get('/api/budget-data').then(res => res.data)
+    return{
+        type: REQUEST_BUDGET_DATA,
+        payload: data
+    }
+}
+
+export function addPurchase(price, description, category){
+    let data = axios.post('/api/budget-data/purchase', {
+        description, 
+        price, 
+        category
+    }).then(res => res.data)
+    return{
+        type: ADD_PURCHASE,
+        payload: data,
+    }
+}
+
+export function removePurchase(id){
+    let data = axios.delete('/api/budget-data/purchase/${id}')
+    .then(res=>res.data)
+    return{
+        type: REMOVE_PURCHSE,
+        payload: data
+    }
+}
+
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        case REQUEST_BUDGET_DATA + "_PENDING":
+            return {...state, loading: true}
+        case REQUEST_BUDGET_DATA + "_FUFILLED":
+            return{...state, ...action.payload, loading: false}
+        case ADD_PURCHASE + "_PENDING":
+            return {...state, loading: true}
+        case ADD_PURCHASE + "_FUFILLED":
+            return{...state, purchases: action.payload, loading: false}
+        case REMOVE_PURCHSE + "_PENDING":
+            return {...state, loading: true}
+        case REMOVE_PURCHSE + "_FUFILLED":
+            return {...state, purchases: action.payload, loading: false}
+        default:
+            return state
+    }
+}
